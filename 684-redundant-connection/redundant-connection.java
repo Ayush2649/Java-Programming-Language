e0fa888ff9df6eaea9d79bfd9048a1f496264503
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2025-01-29 11:56:20 +0530

    Time: 1 ms (75.21%) | Memory: 43.1 MB (86.45%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,38 +1,93 @@
+// class Solution {
+//     public int[] findRedundantConnection(int[][] edges) {
+//         int n = edges.length;
+//         Map<Integer, List<Integer>> map = new HashMap<>();
+
+//         for(int[] edge : edges){
+//             int u = edge[0];
+//             int v = edge[1];
+
+//             if(map.containsKey(u) && map.containsKey(v)){
+//                 boolean[] visited = new boolean[n + 1];
+//                 if(dfs(map, u, v, visited)){
+//                     return edge;
+//                 }
+//             }
+
+//             map.putIfAbsent(u, new ArrayList<>());
+//             map.putIfAbsent(v, new ArrayList<>());
+//             map.get(u).add(v);
+//             map.get(v).add(u);
+//         }
+
+//         return new int[] {};
+//     }
+
+//     public boolean dfs(Map<Integer, List<Integer>> map, int u, int v, boolean[] visited){
+//         visited[u] = true;
+//         if(u == v) return true;
+
+//         for(int neighbour : map.get(u)){
+//             if(visited[neighbour]) continue;
+//             if(dfs(map, neighbour, v, visited)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+class DSU{
+    int[] parent;
+    int[] rank;
+
+    public DSU(int n){
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x){
+        if(x != parent[x]){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void Union(int x, int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) return;
+
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        } else if(rank[y_parent] > rank[x_parent]){
+            parent[x_parent] = y_parent;
+        } else {
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+    }
+}
+
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        DSU dsu = new DSU(n);
 
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
 
-            if(map.containsKey(u) && map.containsKey(v)){
-                boolean[] visited = new boolean[n + 1];
-                if(dfs(map, u, v, visited)){
-                    return edge;
-                }
+            if(dsu.find(u) == dsu.find(v)){
+                return edge;
             }
-
-            map.putIfAbsent(u, new ArrayList<>());
-            map.putIfAbsent(v, new ArrayList<>());
-            map.get(u).add(v);
-            map.get(v).add(u);
+            dsu.Union(u, v);
         }
-
-        return new int[] {};
-    }
-
-    public boolean dfs(Map<Integer, List<Integer>> map, int u, int v, boolean[] visited){
-        visited[u] = true;
-        if(u == v) return true;
-
-        for(int neighbour : map.get(u)){
-            if(visited[neighbour]) continue;
-            if(dfs(map, neighbour, v, visited)){
-                return true;
-            }
-        }
-        return false;
+        return new int[0];
     }
 }
