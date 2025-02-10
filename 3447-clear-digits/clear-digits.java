29316ca57da36a7b3173f1d86cb8a22de6cdb64c
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-02-10 08:23:24 +0530

    Time: 3 ms (38.21%) | Memory: 42.8 MB (33.17%) - LeetSync

--- 3447-clear-digits/clear-digits.java
+++ 3447-clear-digits/clear-digits.java
@@ -1,19 +1,20 @@
 class Solution {
     public String clearDigits(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int i = 0;
+        Stack<Character> stack = new Stack<>();
 
-        while(i < sb.length()){
-            if(Character.isDigit(sb.charAt(i))){
-                sb.deleteCharAt(i);
-                if(i > 0){
-                    sb.deleteCharAt(i - 1);
-                    i--;
-                } 
-            } else {
-                i++;
+        for(char ch : s.toCharArray()){
+            if(ch >= 'a' && ch <= 'z'){
+                stack.push(ch);
+            } else if(!stack.isEmpty()){
+                stack.pop();
             }
         }
-        return sb.toString();
+
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+
+        return result.reverse().toString();
     }
 }
