03598f8cf5782436129e3f3b32e137d9e8b88f3f
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-03-19 17:39:17 +0530

    Time: 5 ms (74.79%) | Memory: 55.3 MB (85.47%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -4,17 +4,17 @@ class Solution {
 
         int flips = 0;
         int flipCountFromPastForCurri = 0;
-        boolean[] isFlipped = new boolean[n];
+        // boolean[] isFlipped = new boolean[n];
 
         for(int i = 0; i < n; i++){
-            if(i >= k && isFlipped[i - k]){
+            if(i >= k && nums[i - k] == 5){
                 flipCountFromPastForCurri--;
             }
 
             if(flipCountFromPastForCurri % 2 == nums[i]){
                 if(i + k > n) return -1;
                 flipCountFromPastForCurri++;
-                isFlipped[i] = true;
+                nums[i] = 5;
                 flips++;
             }
         }
