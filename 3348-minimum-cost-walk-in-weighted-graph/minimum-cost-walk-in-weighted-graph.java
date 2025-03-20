f7776881ce4a8fa0a85fc97e28ed1c655133e7f3
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-03-20 10:33:26 +0530

    Time: 8 ms (60.59%) | Memory: 125.3 MB (30.59%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
@@ -1,55 +1,55 @@
 class Solution {
+    public int find(int x, int[] parent){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x], parent);
+    }
+
+    public void union(int x, int y, int[] parent){
+        parent[y] = x;
+    }
+
     public int[] minimumCost(int n, int[][] edges, int[][] query) {
         int[] parent = new int[n];
         int[] cost = new int[n];
         Arrays.fill(cost, -1);
         int[] result = new int[query.length];
-        
-        for (int i = 0; i < n; i++) {
+
+        for(int i = 0; i < n; i++){
             parent[i] = i;
         }
-        
-        for (int[] edge : edges) {
+
+        for(int[] edge: edges){
             int u = edge[0];
             int v = edge[1];
             int w = edge[2];
-            
+
             int parent_u = find(u, parent);
             int parent_v = find(v, parent);
-            
-            if (parent_u != parent_v) {
+
+            if(parent_u != parent_v){
                 cost[parent_u] &= cost[parent_v];
                 union(parent_u, parent_v, parent);
             }
-            
+
             cost[parent_u] &= w;
         }
-        
-        for (int i = 0; i < query.length; i++) {
+
+        for(int i = 0; i < query.length; i++){
             int s = query[i][0];
-            int t = query[i][1];
-            
-            int p1 = find(s, parent);
-            int p2 = find(t, parent);
-            
-            if (s == t)
+            int d = query[i][1];
+
+            int parent_s = find(s, parent);
+            int parent_d = find(d, parent);
+
+            if(s == d){
                 result[i] = 0;
-            else if (p1 != p2)
+            } else if(parent_s != parent_d){
                 result[i] = -1;
-            else
-                result[i] = cost[p1];
+            } else {
+                result[i] = cost[parent_s];
+            }
         }
-        
+
         return result;
-    }
-    
-    private int find(int x, int[] parent) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x], parent);
-    }
-    
-    private void union(int x, int y, int[] parent) {
-        parent[y] = x;
     }
 }
