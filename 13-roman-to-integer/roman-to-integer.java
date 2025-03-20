611187968fdb7c5c7502600895044006fe842e2e
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-03-20 17:26:03 +0530

    Time: 2 ms (100.00%) | Memory: 44.8 MB (69.40%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,16 +1,18 @@
 class Solution {
     public int romanToInt(String s) {
-        int answer = 0, prev = 0, number = 0;
+        int answer = 0;
+        int number = 0;
+        int prev = 0;
 
-        for(int j = s.length() - 1; j >= 0; j--){
-            switch(s.charAt(j)){
-                case 'M' -> number = 1000;
-                case 'D' -> number = 500;
-                case 'C' -> number = 100;
-                case 'L' -> number = 50;
-                case 'X' -> number = 10;
-                case 'V' -> number = 5;
+        for(int i = s.length() - 1; i >= 0; i--){
+            switch(s.charAt(i)) {
                 case 'I' -> number = 1;
+                case 'V' -> number = 5;
+                case 'X' -> number = 10;
+                case 'L' -> number = 50;
+                case 'C' -> number = 100;
+                case 'D' -> number = 500;
+                case 'M' -> number = 1000;
             }
 
             if(number < prev){
@@ -18,8 +20,10 @@ class Solution {
             } else {
                 answer += number;
             }
+
             prev = number;
         }
+
         return answer;
     }
 }
