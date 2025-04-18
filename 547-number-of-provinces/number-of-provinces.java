18d6f69d893b5759fc5e1df072a54d9de76e9938
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-04-18 10:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (55.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,5 +1,6 @@
 class Solution {
     int n;
+
     public void dfs(int[][] isConnected, int u, boolean[] visited){
         visited[u] = true;
 
@@ -13,30 +14,13 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         n = isConnected.length;
 
-        // // 1. Array to ArrayList
-        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        // for(int i = 0 ; i < V; i++){
-        //     adj.add(new ArrayList<>());
-        // }
-
-        // for(int i = 0; i < V; i++){
-        //     for(int j = 0; j < V; j++){
-        //         if(isConnected[i][j] == 1 && i != j){
-        //             adj.get(i).add(j);
-        //             adj.get(j).add(i);
-        //         }
-        //     }
-        // }
-
-        // 2. Normal dfs
         boolean[] visited = new boolean[n];
         int count = 0;
 
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                count++;
                 dfs(isConnected, i, visited);
+                count++;
             }
         }
 
