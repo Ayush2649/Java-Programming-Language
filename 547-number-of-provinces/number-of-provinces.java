89134cca04c1a21f622b563154a4507c27ff9ec7
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-04-18 09:41:48 +0530

    Time: 1 ms (89.34%) | Memory: 48 MB (36.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,41 +1,42 @@
 class Solution {
-    public void dfs(ArrayList<ArrayList<Integer>> adj, int curr, boolean[] visited){
-        visited[curr] = true;
+    int n;
+    public void dfs(int[][] isConnected, int u, boolean[] visited){
+        visited[u] = true;
 
-        for(int nbr: adj.get(curr)){
-            if(!visited[nbr]){
-                dfs(adj, nbr, visited);
+        for(int v = 0; v < n; v++){
+            if(!visited[v] && isConnected[u][v] == 1){
+                dfs(isConnected, v, visited);
             }
         }
     }
 
     public int findCircleNum(int[][] isConnected) {
-        int V = isConnected.length;
+        n = isConnected.length;
 
-        // 1. Array to ArrayList
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        // // 1. Array to ArrayList
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i = 0 ; i < V; i++){
-            adj.add(new ArrayList<>());
-        }
+        // for(int i = 0 ; i < V; i++){
+        //     adj.add(new ArrayList<>());
+        // }
 
-        for(int i = 0; i < V; i++){
-            for(int j = 0; j < V; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
+        // for(int i = 0; i < V; i++){
+        //     for(int j = 0; j < V; j++){
+        //         if(isConnected[i][j] == 1 && i != j){
+        //             adj.get(i).add(j);
+        //             adj.get(j).add(i);
+        //         }
+        //     }
+        // }
 
         // 2. Normal dfs
-        boolean[] visited = new boolean[V];
+        boolean[] visited = new boolean[n];
         int count = 0;
 
-        for(int i = 0; i < V; i++){
+        for(int i = 0; i < n; i++){
             if(!visited[i]){
                 count++;
-                dfs(adj, i, visited);
+                dfs(isConnected, i, visited);
             }
         }
 
