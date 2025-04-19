6d890b563c7454573782ef40c389af2790f97c32
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-04-19 10:33:07 +0530

    Time: 2 ms (99.25%) | Memory: 48.6 MB (6.50%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,19 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
+        int ele1 = -1, ele2 = -1;
+        int cnt1 = 0, cnt2 = 0;
 
-        Map<Integer, Integer> freqMap = new HashMap<>();
-
-        for(int num : nums){
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        for(int ele : nums){
+            if(ele1 == ele){
+                cnt1++;
+            } else if (ele2 == ele){
+                cnt2++;
+            } else if(cnt1 == 0){
+                ele1 = ele;
+                cnt1++;
+            } else if(cnt2 == 0){
+                ele2 = ele;
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
         }
 
         List<Integer> res = new ArrayList<>();
+        cnt1 = 0;
+        cnt2 = 0;
 
-        for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
-            if(entry.getValue() > n/3){
-                res.add(entry.getKey());
-            }
+        for(int ele : nums){
+            if(ele1 == ele) cnt1++;
+            if(ele2 == ele) cnt2++;
+        }
+
+        if(cnt1 > n / 3) res.add(ele1);
+        if(cnt2 > n / 3 && ele1 != ele2) res.add(ele2);
+
+        if(res.size() == 2 && res.get(0) > res.get(1)){
+            int temp = res.get(0);
+            res.set(0, res.get(1));
+            res.set(1, temp);
         }
 
         return res;
