ae8fb93cb9398bff218f3dfe46f7bbb84bf17b68
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-04-19 10:03:34 +0530

    Time: 4 ms (21.74%) | Memory: 46.9 MB (87.38%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,7 +1,6 @@
 class Solution {
     int n;
-    
-    public void bfs(int[][] isConnected, boolean[] visited, int curr){
+    public void bfs(ArrayList<ArrayList<Integer>> adj, int curr, boolean[] visited){
         Queue<Integer> q = new LinkedList<>();
 
         q.offer(curr);
@@ -10,8 +9,8 @@ class Solution {
         while(!q.isEmpty()){
             int u = q.poll();
 
-            for(int v = 0; v < n; v++){
-                if(!visited[v] && isConnected[u][v] == 1){
+            for(int v : adj.get(u)){
+                if(!visited[v]){
                     q.offer(v);
                     visited[v] = true;
                 }
@@ -22,27 +21,27 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         n = isConnected.length;
 
-        // ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        // for(int i = 0; i < n; i++){
-        //     adj.add(new ArrayList<>());
-        // }
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
 
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < n; j++){
-        //         if(isConnected[i][j] == 1){
-        //             adj.get(i).add(j);
-        //             adj.get(j).add(i);
-        //         }
-        //     }
-        // }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(isConnected[i][j] == 1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
 
         boolean[] visited = new boolean[n];
-        int count = 0;
+        int count  = 0;
 
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                bfs(isConnected, visited, i);
+                bfs(adj, i, visited);
                 count++;
             }
         }
