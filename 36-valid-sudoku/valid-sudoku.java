d9830676a3450754b0dad1e7049cb2aded2dc737
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-08-30 08:08:22 +0530

    Time: 13 ms (20.67%) | Memory: 45 MB (23.53%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,46 +1,22 @@
 class Solution {
-    public boolean isValidSub(char[][] board, int sr, int er, int sc, int ec){
-        Set<Character> st = new HashSet<>();
-        for(int row = sr; row <= er; row++){
-            for(int col = sc; col <= ec; col++){
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.contains(ch)) return false;
-                st.add(ch);
-            }
-        }
-        return true;
-    }
-
     public boolean isValidSudoku(char[][] board) {
-        // Validate Rows
-        for(int row = 0; row < 9; row++){
-            Set<Character> st = new HashSet<>();
-            for(int col = 0; col < 9; col++){
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.contains(ch)) return false;
-                st.add(ch);
-            } 
-        }
+        Set<String> seen = new HashSet<>();
 
-        // Validate Cols
-        for(int col = 0; col < 9; col++){
-            Set<Character> st = new HashSet<>();
-            for(int row = 0; row < 9; row++){
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(st.contains(ch)) return false;
-                st.add(ch);
-            } 
-        }
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.') continue;
 
-        // Validate Boxes
-        for(int sr = 0; sr < 9; sr += 3){
-            int er = sr + 2;
-            for(int sc = 0; sc < 9; sc += 3){
-                int ec = sc + 2;
-                if(!isValidSub(board, sr, er, sc, ec)) return false;
+                char ch = board[i][j];
+
+                String row = ch + "_row_" + i;
+                String col = ch + "_col_" + j;
+                String box = ch + "_box_" + (i / 3) + "_" + (j / 3);
+
+                if(seen.contains(row) || seen.contains(col) || seen.contains(box)) return false;
+
+                seen.add(row);
+                seen.add(col);
+                seen.add(box);
             }
         }
 
