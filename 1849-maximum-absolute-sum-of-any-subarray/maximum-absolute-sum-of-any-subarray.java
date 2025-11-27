b4ff447a9bba508e1ddfaffc99ea28c6241b5746
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-11-27 17:26:22 +0530

    Time: 4 ms (88.46%) | Memory: 66.7 MB (26.57%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -1,16 +1,21 @@
 class Solution {
-
     public int maxAbsoluteSum(int[] nums) {
-        int minPrefixSum = 0, maxPrefixSum = 0;
-        int prefixSum = 0;
+        int currSubArraySum = nums[0];
+        int maxSubArraySum = nums[0];
+        int minSubArraySum = nums[0];
 
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];
-
-            minPrefixSum = Math.min(minPrefixSum, prefixSum);
-            maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
+        for(int i = 1; i < nums.length; i++){
+            currSubArraySum = Math.max(nums[i], nums[i] + currSubArraySum);
+            maxSubArraySum = Math.max(currSubArraySum, maxSubArraySum);
         }
 
-        return maxPrefixSum - minPrefixSum;
+        currSubArraySum = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            currSubArraySum = Math.min(nums[i], nums[i] + currSubArraySum);
+            minSubArraySum = Math.min(currSubArraySum, minSubArraySum);
+        }
+
+        return Math.max(Math.abs(maxSubArraySum), Math.abs(minSubArraySum));
     }
 }
