71d50f3c2e12dd3e37e024c1712e0ab11eb7af5e
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2025-12-26 11:42:04 +0530

    Time: 11 ms (75.86%) | Memory: 46.9 MB (63.62%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -2,38 +2,25 @@ class Solution {
     public int bestClosingTime(String customers) {
         int n = customers.length();
 
-        int[] prefixN = new int[n + 1];
-        Arrays.fill(prefixN, 0);
-
-        int[] suffixY = new int[n + 1];
-        Arrays.fill(suffixY, 0);
-
-        for(int i = 1; i <= n; i++){
-            if(customers.charAt(i - 1) == 'N'){
-                prefixN[i] = prefixN[i - 1] + 1;
-            } else {
-                prefixN[i] = prefixN[i - 1];
-            }
+        int minHour = 0;
+        int penalty = 0;
+        for(int i = 0; i < n; i++){
+            if(customers.charAt(i) == 'Y') penalty++;
         }
 
-        for(int i = n - 1; i >= 0; i--){
+        int minPenalty = penalty;
+
+        for(int i = 0; i < n; i++){
             if(customers.charAt(i) == 'Y'){
-                suffixY[i] = suffixY[i + 1] + 1;
+                penalty--;
             } else {
-                suffixY[i] = suffixY[i + 1];
+                penalty++;
             }
-        }
 
-        int minPenalty = Integer.MAX_VALUE;
-        int minHour = Integer.MAX_VALUE;
-
-        for(int i = 0; i <= n; i++){
-            int currPenalty = prefixN[i] + suffixY[i];
-
-            if(currPenalty < minPenalty){
-                minPenalty = currPenalty;
-                minHour = i;
-            } 
+            if(penalty < minPenalty){
+                minPenalty = penalty;
+                minHour = i + 1;
+            }
         }
 
         return minHour;
