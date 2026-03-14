4768de7d5815dcec8ec016a6b4e484bb6be2008e
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2026-03-14 17:56:13 +0530

    Time: 22 ms (23.54%) | Memory: 46.9 MB (31.81%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,31 +1,36 @@
 class Solution {
-    public void solve(int n, StringBuilder curr, List<String> result){
-        if(curr.length() == n){
-            result.add(curr.toString());
+
+    List<String> happyStrings = new ArrayList<>();
+
+    public String getHappyString(int n, int k) {
+        String currentString = "";
+        // Generate all happy strings of length n
+        generateHappyStrings(n, currentString);
+
+        // Check if there are at least k happy strings
+        if (happyStrings.size() < k) return "";
+
+        return happyStrings.get(k - 1);
+    }
+
+    protected void generateHappyStrings(int n, String currentString) {
+        // If the current string has reached the desired length, add it to the
+        // list
+        if (currentString.length() == n) {
+            happyStrings.add(currentString);
             return;
         }
 
-        for(char ch = 'a'; ch <= 'c'; ch++){
-            if(curr.length() > 0 && curr.charAt(curr.length() - 1) == ch) continue;
+        // Try adding each character ('a', 'b', 'c') to the current string
+        for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
+            // Skip if the current character is the same as the last character
+            if (
+                currentString.length() > 0 &&
+                currentString.charAt(currentString.length() - 1) == currentChar
+            ) continue;
 
-            // Do
-            curr.append(ch);
-
-            // Explore
-            solve(n, curr, result);
-
-            // Undo
-            curr.deleteCharAt(curr.length() - 1);
+            // Recursively generate the next character
+            generateHappyStrings(n, currentString + currentChar);
         }
-    }
-
-    public String getHappyString(int n, int k) {
-        StringBuilder curr = new StringBuilder();
-        List<String> result = new ArrayList<>();
-        solve(n, curr, result);
-
-        if(result.size() < k) return "";
-
-        return result.get(k - 1);
     }
 }
