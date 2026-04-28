7682b56de62aa39b2f70be8012ebdd2fb943b1d8
Ayush Sahu <142195140+Ayush2649@users.noreply.github.com>
2026-04-28 10:02:47 +0530

    Time: 144 ms (14.50%) | Memory: 116.1 MB (18.32%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,24 +1,32 @@
 class Solution {
-    public int minOperations(int[][] grid, int x) {
-        int m = grid.length;
-        int n = grid[0].length;
 
-        List<Integer> nums = new ArrayList<>();
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                nums.add(grid[i][j]);
+    public int minOperations(int[][] grid, int x) {
+        // Create a list to store all the numbers from the grid
+        ArrayList<Integer> numsArray = new ArrayList<>();
+        int result = 0;
+
+        // Flatten the grid into numsArray
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                numsArray.add(grid[row][col]);
             }
         }
 
-        Collections.sort(nums);
-        int target = nums.get(nums.size() / 2);
+        // Sort numsArray in non-decreasing order to easily find the median
+        Collections.sort(numsArray);
 
-        int result = 0;
-        for(int num : nums){
-            if(num % x != target % x){
+        int length = numsArray.size();
+        // Store the median element as the final common value
+        int finalCommonNumber = numsArray.get(length / 2);
+
+        // Iterate through each number in numsArray
+        for (int number : numsArray) {
+            // If the remainder when divided by x is different, return -1
+            if (number % x != finalCommonNumber % x) {
                 return -1;
-            } 
-            result += Math.abs(target - num) / x;
+            }
+            // Add the number of operations required to make the current number equal to finalCommonNumber
+            result += Math.abs(finalCommonNumber - number) / x;
         }
 
         return result;
